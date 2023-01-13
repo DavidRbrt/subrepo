7909bdd49e3345b483c052df4d2844c15d9d49e8
David ROBERT <robert.david.mickael@gmail.com>
2023-01-13 18:47:52 +0100

    working without default param

--- subrepo.py
+++ subrepo.py
@@ -3,29 +3,77 @@
 import argparse
 import os
 import pathlib
+import json
+from collections import OrderedDict
+import jsonschema
+
+# Expected schema for subrepos.json
+# see https://json-schema.org/learn/getting-started-step-by-step to edit it
+json_schema = {
+    'title': 'subrepos.json',
+    'type': 'object',
+    'properties': {
+        'default': {
+            'type' : 'object',
+            'properties':{
+                'revision': { 'type': 'string' },
+                'local_path': { 'type': 'string' },
+            },
+        },
+        'list': {
+            'type': 'array',
+            'items': {
+                'type': 'object',
+                'properties': {
+                    'repo_path': { 'type': 'string' },
+                    'revision': { 'type': 'string' },
+                    'local_path': { 'type': 'string' },
+                },
+                'required': [
+                    'repo_path',
+                ],
+            },
+            'minItems': 0,
+            'uniqueItems': True,
+        }
+    },
+    'required': [
+        'list',
+    ],
+}
 
 class Subrepo:
-    """Subrepo object"""
-    def __init__(self, repo_path, rev='master', local_path='.'):
+    def __init__(self, repo_path, revision='master', local_path='.'):
         self.repo_path = repo_path
-        self.rev = rev
+        self.revision = revision
         self.local_path = local_path
         #
         self.repo_name = pathlib.Path(repo_path).stem
 
+def open_json(file_path):
+    try:
+        data = json.load(open(file_path, 'r'))
+    except FileNotFoundError as e:
+        print(f'ERROR: Cannot open file {file_path} ({e})')
+    except ValueError as e:
+        print(f'ERROR: File {file_path} is not a valid json ({e})')
 
-def parse_subrepo_list():
+    try:
+        jsonschema.validate(instance=data, schema=json_schema)
+    except jsonschema.exceptions.ValidationError as e:
+        print(f'ERROR: File {file_path} format is invalid: {e.message}')
+
+    return data
+
+
+def parse_subrepo_list(list):
     subrepo_list = []
 
-    repo_path = 'git@github.com:DavidRbrt/dev-tools.git'
-    rev = 'f303925686fc87ab2547bfee691d1d8be00f4b71'
-    local_path = 'subrepos/yoyoyo'
-    subrepo_list.append(Subrepo(repo_path, rev, local_path))
-
-    repo_path = 'git@github.com:DavidRbrt/lookup-table-generator.git'
-    rev = '798a91585f3ad29a9c28cad8a3c35ee725538aed'
-    local_path = 'subrepos'
-    subrepo_list.append(Subrepo(repo_path, rev, local_path))
+    for element in list:
+        repo_path = element['repo_path']
+        revision = element['revision']
+        local_path = element['local_path']
+        subrepo_list.append(Subrepo(repo_path, revision, local_path))
 
     return subrepo_list
 
@@ -36,14 +84,14 @@ def fetch_all(base_dir, subrepo_list):
         os.chdir(subrepo.local_path)
         os.system(f'git clone {subrepo.repo_path}')
         os.chdir(subrepo.repo_name)
-        os.system(f'git checkout {subrepo.rev}')
+        os.system(f'git checkout {subrepo.revision}')
         os.chdir(base_dir)
 
 
 def command_line_parser():
-	parser = argparse.ArgumentParser(description="subrepo")
+	parser = argparse.ArgumentParser(description='subrepo')
 
-	parser.add_argument('-u', '--update', required=False, default=False, action='store_true', help="fetch all subrepos")
+	parser.add_argument('-u', '--update', required=False, default=False, action='store_true', help='fetch all subrepos')
 
 	args, _ = parser.parse_known_args()
 
@@ -55,12 +103,15 @@ def main():
 
     base_dir = os.path.realpath(os.path.dirname(__file__))
 
-    subrepo_list = parse_subrepo_list()
+    data = open_json('subrepos.json')
+    subrepo_list = parse_subrepo_list(data['list'])
+
+    # TODO: use default json param
 
     if update:
-        print("fetching all subrepos ...")
+        print('fetching all subrepos ...')
         fetch_all(base_dir, subrepo_list)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
